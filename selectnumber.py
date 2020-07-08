0c823df00033cb80d8f5173a93bc75f79b9f2f74
ROUJINN <64689938+ROUJINN@users.noreply.github.com>
2020-07-08 13:20:36 +0800

    Add files via upload

---
+++ selectnumber.py
@@ -0,0 +1,10 @@
+import random
+f = True
+while f:
+    a=random.randint(1,49)
+    if a != 13:
+        print(a)               
+        break
+    else:
+        continue         # 回到条件语句
+# 完成版终于成功啦    2020/5/30
